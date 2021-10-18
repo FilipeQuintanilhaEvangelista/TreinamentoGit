dc67507608d7f2001585edf5f3a76870367b6ca7
Filipe Quintanilha <57538817+FilipeQuintanilhaEvangelista@users.noreply.github.com>
2021-10-18 17:50:17 -0300

    Commit do formulario

--- formulario.php
+++ formulario.php
@@ -1,3 +1,18 @@
 <?php
+$num1 =  $_POST["campo1"];
+$num2 =  $_POST["campo2"];
 
+$soma = $num1 + $num2;
+
+$multiplicacao = $num1 * $num2;
+
+$divisao = $num1 / $num2;
+
+$subtracao = $num1 - $num2;
+
+
+echo "O valor da soma é: $soma";
+echo "<br>O valor da divisão é: $divisao " ;
+echo "<br>O valor da multiplicacao é: $multiplicacao";
+echo "<br>O valor da subtracao é: $subtracao";
 ?>
